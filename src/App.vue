73e0bdff7416f81afdb21a430e8ffbc3da1549cb
rasskazov-pv <rasskazov.paul@yandex.ru>
2021-04-13 02:47:40 +0300

    Добавлена форма входа, метод входа, роутер для страниц

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,30 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <div class="container">
+      <b-navbar>
+        <template #brand>
+          <b-navbar-item tag="router-link" :to="{ path: '/' }">
+            <h1>Long tic tac toe</h1>
+          </b-navbar-item>
+        </template>
+
+        <template #end>
+          <b-navbar-item tag="div">
+            <div class="buttons">
+              <a class="button is-primary">
+                <strong>Регистрация</strong>
+              </a>
+              <router-link to="/login" class="button is-light">
+                Войти
+              </router-link>
+
+            </div>
+          </b-navbar-item>
+        </template>
+      </b-navbar>
+
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
--- src/components/login/index.vue
+++ src/components/login/index.vue
@@ -1,3 +1,33 @@
 <template>
-  <h2>Login page</h2>
+  <div class="columns">
+    <div class="column is-4 is-offset-4">
+      <form @submit.prevent="signIn">
+        <b-field label="Логин">
+          <b-input v-model="login"></b-input>
+        </b-field>
+
+        <b-field label="Пароль">
+          <b-input type="password" password-reveal v-model="password"></b-input>
+        </b-field>
+
+        <b-button native-type="submit">Войти</b-button>
+      </form>
+    </div>
+  </div>
 </template>
+
+<script>
+  export default {
+    data: () => {
+      return {
+        login: '',
+        password: ''
+      }
+    },
+    methods: {
+      signIn: function () {
+        console.log(this.login, this.password);
+      }
+    }
+  }
+</script>
