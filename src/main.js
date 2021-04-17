0167f9de6e6dc0f5551f7f1247353a0c2991d231
rasskazov-pv <rasskazov.paul@yandex.ru>
2021-04-17 20:53:29 +0300

    Тест

--- src/main.js
+++ src/main.js
@@ -4,10 +4,21 @@ import Buefy from "buefy"
 import 'buefy/dist/buefy.css'
 
 import router from "../router"
+import axios from "axios";
 
 Vue.use(Buefy);
 
 Vue.config.productionTip = false
+
+axios.defaults.xsrfCookieName = "CSRF-TOKEN";
+axios.defaults.xsrfHeaderName = "X-CSRF-Token";
+axios.defaults.withCredentials = true;
+
+axios.get("http://5a6297658517.ngrok.io/login?login=pavel&password=pavel")
+    .then(() => {
+        
+    });
+// axios.delete("http://5a6297658517.ngrok.io/logout");
 
 new Vue({
   render: h => h(App),
