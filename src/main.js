710dd2c86f4e807909abba030d7778a94d76df78
Антон Крестьянинов <akrestyaninov@rzdit.ru>
2021-04-26 19:57:04 +0300

    Добавлено ясности в main.js и router.js + подключен Vuex

--- src/main.js
+++ src/main.js
@@ -1,16 +1,25 @@
-import App    from './App.vue'
-import Buefy  from 'buefy'
-import router from './router'
-import Vue    from 'vue'
+// ----- Библиотеки
+import Buefy from 'buefy';
+import Vue   from 'vue';
 
-import 'buefy/dist/buefy.css'
-import './styles/application.css'
+// ----- JS и Vue файлы
+import App    from './App.vue';
+import router from './router';
+import store  from './store';
 
+// ----- CSS файлы
+import 'buefy/dist/buefy.css';
+import './styles/application.css';
+
+// ----- Подключение библиотек
 Vue.use(Buefy);
 
-Vue.config.productionTip = false
+// ----- Доп конфигурации
+Vue.config.productionTip = false;
 
+// ----- Создание приложения
 new Vue({
   render: h => h(App),
-  router
-}).$mount('#app')
+  router,
+  store,
+}).$mount('#app');
--- src/router.js
+++ src/router.js
@@ -1,21 +1,26 @@
-import Vue       from 'vue'
-import VueRouter from "vue-router"
-
-import Home           from './components/home'
-import Login          from './components/login'
-import CurrentProfile from './components/current/profile'
-
-const routes = [
-  { path: '/', component: Home },
-  { path: '/login', component: Login },
-  { path: '/current', component: CurrentProfile }
-];
-
-const router = new VueRouter({
-  routes,
-  mode: 'history'
-});
+// ----- Импорт и использование библиотеки
+import Vue       from 'vue';
+import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
+// ----- Файлы компонентов
+import Home           from './components/home';
+import Login          from './components/login';
+import CurrentProfile from './components/current/profile';
+
+// ----- Описание роутов
+const routes = [
+  { path: '/', component: Home },
+  { path: '/login', component: Login },
+  { path: '/current', component: CurrentProfile },
+];
+
+// ----- Создание роутов
+const router = new VueRouter({
+  routes,
+  mode: 'history',
+});
+
+// ----- /барабанная дробь/ Экспорт роутов!
 export default router;
