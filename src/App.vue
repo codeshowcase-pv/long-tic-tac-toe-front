73446a688af03be559899a70441b7738f2cf4e89
Антон Крестьянинов <9593795@gmail.com>
2021-04-27 14:24:03 +0300

    Классы для центровки содержимого вынесены в App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" class="h-100">
+  <div id="app" class="h-100 is-flex is-flex-direction-column is-justify-content-center is-align-items-center">
     <router-view></router-view>
   </div>
 </template>
--- src/components/home/index.vue
+++ src/components/home/index.vue
@@ -1,25 +1,23 @@
 <template>
-  <section class="h-100 is-flex is-flex-direction-column is-justify-content-center is-align-items-center">
-    <div class="tile is-ancestor is-4 box py-3">
-      <div class="tile is-parent is-vertical is-align-items-center">
-        <div class="tile is-child">
-          <b-button tag="router-link"
-                    to="/login"
-                    type="is-primary">
-            Войти
-          </b-button>
-        </div>
+  <div class="tile is-ancestor is-4 box py-3">
+    <div class="tile is-parent is-vertical is-align-items-center">
+      <div class="tile is-child">
+        <b-button tag="router-link"
+                  to="/login"
+                  type="is-primary">
+          Войти
+        </b-button>
+      </div>
 
-        <div class="tile is-child">
-          <b-button tag="router-link"
-                    to="/registration"
-                    type="is-info">
-            Зарегистрироваться (бесплатно без смс)
-          </b-button>
-        </div>
+      <div class="tile is-child">
+        <b-button tag="router-link"
+                  to="/registration"
+                  type="is-info">
+          Зарегистрироваться (бесплатно без смс)
+        </b-button>
       </div>
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
