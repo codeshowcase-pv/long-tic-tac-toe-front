6fbfac19b05de2de95f0787cb7c90ecab64108af
Антон Крестьянинов <9593795@gmail.com>
2021-04-30 02:02:04 +0300

    Третий этап переноса аутентификации с кукесов на jwt
    1. Добавлена грубая реализация обновления истёкшего access-токена

--- src/api.js
+++ src/api.js
@@ -10,17 +10,43 @@ const api = axios.create({
 
 function nonLogoutRequest(config) { return config.url !== '/logout'; }
 
-api.interceptors.request.use(function (config) {
-  if (store.state.loggedIn) config.headers['Authorization'] = `Bearer ${ localStorage.accessToken }`;
-  return config;
-}, null, { runWhen: nonLogoutRequest });
+api.interceptors.request.use(
+  function (config) {
+    if (store.state.loggedIn) config.headers['Authorization'] = `Bearer ${ localStorage.accessToken }`;
+    return config;
+  },
+  null,
+  { runWhen: nonLogoutRequest },
+);
 
-function logoutRequest(config) { return config.url === '/logout'; }
+function requestWithRefreshToken(config) { return config.url === '/logout' || config.url === '/refresh'; }
 
-api.interceptors.request.use(function (config) {
-  if (store.state.loggedIn) config.headers['X-Refresh-Token'] = localStorage.refreshToken;
-  return config;
-}, null, { runWhen: logoutRequest });
+api.interceptors.request.use(
+  function (config) {
+    if (store.state.loggedIn) config.headers['X-Refresh-Token'] = localStorage.refreshToken;
+    return config;
+  },
+  null,
+  { runWhen: requestWithRefreshToken },
+);
+
+api.interceptors.response.use(
+  null,
+  function (error) {
+    const originalRequest = error.config;
+    if (error.response.data.code === 'access_token_expired') {
+      return axios.request({
+        method:  'post',
+        url:     `${ process.env.VUE_APP_API_BASE_URL }/refresh`,
+        headers: { 'X-Refresh-Token': localStorage.refreshToken },
+      }).then(({ data }) => {
+        localStorage.setItem('accessToken', data.access);
+        originalRequest.headers['Authorization'] = `Bearer ${ localStorage.accessToken }`;
+        return axios(originalRequest);
+      });
+    }
+  },
+);
 
 export const login = (login, password) => {
   return api.post(`/login`, {
